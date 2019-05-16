8855e58c7f94ab364eadbe20e238bd9f46fd2875
navjeet7894 <=>
2019-05-16 16:39:49 -0400

    first commit

---
+++ javascriptfile.js
@@ -0,0 +1,9 @@
+let a=2;
+let b=2;
+let firstname="Joe";
+let lastname="Smith";
+
+let item1price=20;
+let item2price=30;
+let sum=item1price+item2price;
+alert("HELLO, "+firstname+" "+lastname+" "+"your total is : "+sum);
